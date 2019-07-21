5ca2904fd23e8f65fea927e7d0c48cafc66fef5e
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-07-21 16:41:23 +0400

    redux-thunk and redux-loading-promise middlewares added to fix reducers issue with returning promises instead of final data

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
+import promise from 'redux-loading-promise-middleware';
+
+const middlewares = applyMiddleware(thunk, promise);
 
 export default function initializeStore(initData) {
-  return createStore(reducers, initData);
+  return createStore(reducers, initData, middlewares);
 }
